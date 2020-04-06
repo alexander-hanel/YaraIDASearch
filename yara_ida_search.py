c0d67d3dc53770975da40d2795438e0078d00a83
unknown <alexander.hanel@gmail.com>
2020-04-06 17:52:28 -0600

    repo created

---
+++ yara_ida_search.py
@@ -0,0 +1,106 @@
+import yara
+import operator
+import idautils
+
+SEARCH_CASE = 4
+SEARCH_REGEX = 8
+SEARCH_NOBRK = 16
+SEARCH_NOSHOW = 32
+SEARCH_UNICODE = 64
+SEARCH_IDENT = 128
+SEARCH_BRK = 256
+
+class YaraIDASearch:
+    def __init__(self):
+        self.mem_results = ""
+        self.mem_offsets = []
+        if not self.mem_results:
+            self._get_memory()
+
+    def _get_memory(self):
+        print("Status: Loading memory for Yara.")
+        result = b""
+        segments_starts = [ea for ea in idautils.Segments()]
+        offsets = []
+        start_len = 0
+        for start in segments_starts:
+            end = idc.get_segm_end(start)
+            result += idc.get_bytes(start, end - start)
+            offsets.append((start, start_len, len(result)))
+            start_len = len(result)
+        print("Status: Memory has been loaded.")
+        self.mem_results = result
+        self.mem_offsets = offsets
+
+    def _to_virtual_address(self, offset, segments):
+        va_offset = 0
+        for seg in segments:
+            if seg[1] <= offset < seg[2]:
+                va_offset = seg[0] + (offset - seg[1])
+        return va_offset
+
+    def _init_sig(self, sig_type, pattern, sflag):
+        if SEARCH_REGEX & sflag:
+            signature = "/%s/" % pattern
+            if SEARCH_CASE & sflag:
+                # ida is not case sensitive by default but yara is
+                pass
+            else:
+                signature += " nocase"
+            if SEARCH_UNICODE & sflag:
+                signature += " wide"
+        elif sig_type == "binary":
+            signature = " %s " % pattern
+        elif sig_type == "text" and (SEARCH_REGEX & sflag) == False:
+            signature = '"%s"' % pattern
+            if SEARCH_CASE & sflag:
+                pass
+            else:
+                signature += " nocase"
+            signature += " wide ascii"
+        yara_rule = "rule foo : bar { strings: $a = %s condition: $a }" % signature
+        return yara_rule
+
+    def _compile_rule(self, signature):
+        try:
+            rules = yara.compile(source=signature)
+        except Exception as e:
+            print("ERROR: Cannot compile Yara rule %s" % e)
+            return False, None
+        return True, rules
+
+    def _search(self, signature):
+        status, rules = self._compile_rule(signature)
+        if not status:
+            return False, None
+        values = []
+        matches = rules.match(data=self.mem_results)
+        if not matches:
+            return False, None
+        for rule_match in matches:
+            for match in rule_match.strings:
+                match_offset = match[0]
+                values.append(self._to_virtual_address(match_offset, self.mem_offsets))
+        return values
+
+    def find_binary(self, bin_str, sflag=0):
+        yara_sig = self._init_sig("binary", bin_str, sflag)
+        offset_matches = self._search(yara_sig)
+        return offset_matches
+
+    def find_text(self, q_str, sflag=0):
+        yara_sig = self._init_sig("text", q_str, sflag)
+        offset_matches = self._search(yara_sig)
+        return offset_matches
+
+    def find_sig(self, yara_rule):
+        # recommendation/fork by R3MRUM https://github.com/R3MRUM
+        offset_matches = self._search(yara_rule)
+        return offset_matches
+
+    def reload_scan_memory(self):
+        self._get_memory()
+        
+
+y = YaraIDASearch()
+print(y.find_text("GetProcAddress"))
